c475fb9b60d494d49f30745c693d4e9b2b800205
Aniket Kumar <aniketpundir348@gmail.com>
2025-05-17 21:41:52 +0530

    add restaurent randamly

--- src/components/RestaurentAdd/RestaurentCard/RestaurentCard.jsx
+++ src/components/RestaurentAdd/RestaurentCard/RestaurentCard.jsx
@@ -1,13 +1,34 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./RestaurentCard.css";
 import { Storecontext } from "../../../context/Storecontext";
+import axios from "axios";
 
 const RestaurantCard = () => {
-  const { restroRes } = useContext(Storecontext);
+  const { restroRes, url } = useContext(Storecontext);
+  const [allRestro, setAllRestro] = useState([]);
   const navigate = useNavigate();
 
-  console.log(restroRes);
+  const location = async () => {
+    try {
+      let newUrl = url;
+      newUrl += "/api/restro/all";
+
+      console.log(newUrl);
+
+      const res = await axios.get(newUrl);
+
+      setAllRestro(res.data.data);
+    } catch {
+      setAllRestro([]);
+    }
+  };
+
+  useEffect(() => {
+    location();
+  }, []);
+
+  console.log(allRestro);
 
   return (
     <>
@@ -16,33 +37,53 @@ const RestaurantCard = () => {
       </div>
 
       <div className="restaurentCard-container">
-        {restroRes && Array.isArray(restroRes) ? (
-          restroRes.map((items, index) => (
-            <div key={index} className="card-details" id={items._id}>
-              <div className="rest-img">
-                <img src={items.image} alt="Restaurant" />
-              </div>
-              <div className="rest-text">
-                <h3 className="restaurent-name">{items.name}</h3>
-                <div className="location-menu_button">
-                  <span className="location">
-                    {items.address?.district} <br /> {items.address?.locality}
-                  </span>
-                  <button
-                    onClick={() => {
-                      navigate(`/menu/${items._id}`);
-                      window.scroll(0, -10);
-                    }}
-                  >
-                    View Menu
-                  </button>
+        {restroRes && Array.isArray(restroRes)
+          ? restroRes.map((items, index) => (
+              <div key={index} className="card-details" id={items._id}>
+                <div className="rest-img">
+                  <img src={items.image} alt="Restaurant" />
+                </div>
+                <div className="rest-text">
+                  <h3 className="restaurent-name">{items.name}</h3>
+                  <div className="location-menu_button">
+                    <span className="location">
+                      {items.address?.district} <br /> {items.address?.locality}
+                    </span>
+                    <button
+                      onClick={() => {
+                        navigate(`/menu/${items._id}`);
+                        window.scroll(0, -10);
+                      }}
+                    >
+                      View Menu
+                    </button>
+                  </div>
                 </div>
               </div>
-            </div>
-          ))
-        ) : (
-          <p>No Restaurants add in your location</p>
-        )}
+            ))
+          : allRestro.map((items, index) => (
+              <div key={index} className="card-details" id={items._id}>
+                <div className="rest-img">
+                  <img src={items.image} alt="Restaurant" />
+                </div>
+                <div className="rest-text">
+                  <h3 className="restaurent-name">{items.name}</h3>
+                  <div className="location-menu_button">
+                    <span className="location">
+                      {items.address?.district} <br /> {items.address?.locality}
+                    </span>
+                    <button
+                      onClick={() => {
+                        navigate(`/menu/${items._id}`);
+                        window.scroll(0, -10);
+                      }}
+                    >
+                      View Menu
+                    </button>
+                  </div>
+                </div>
+              </div>
+            ))}
       </div>
     </>
   );
